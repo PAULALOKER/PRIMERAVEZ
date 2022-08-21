7bc38d8a7627207d17623e7aa461e6f2846b9215
paula loker <ppau.l@live.com.ar>
2022-08-21 18:54:23 -0300

    cambio de prueba

--- PRIMERAVEZ/Program.cs
+++ PRIMERAVEZ/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Buen día");
+Console.WriteLine("Buenas tardes");
+Console.WriteLine("Buenas tardes");
